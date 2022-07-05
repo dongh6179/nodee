c8fa169db2d240ea6df06bb4118c6c0c7692778a
dongh6179 <dongh6179@gmail.com>
2022-07-05 13:50:45 -0400

    added more packages

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-catMe();
+// const { Board } = require('minesweeper-board');
+
+// const board = new Board(5, 5); //dimension, mines
+// console.log(board.getBoard());
+
+
+// var binary = require('text-to-binary-converter');
+// console.log(binary.convert("binary"));
